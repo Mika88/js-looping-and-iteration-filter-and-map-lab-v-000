e6d27a111c0544068a966a6f7b9c2924c795a2b1
Mika88 <mikaspivak@gmail.com>
2019-03-26 21:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
-function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function (driver) {
+function driversWithRevenueOver(list, revenue) {
+  return list.filter(function (driver) {
     return driver.revenue === revenue;
   })
 }
